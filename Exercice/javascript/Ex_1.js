47bb755435fd8182c00e54b7ecb60bb0c672ff33
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-10-04 14:26:13 +0200

    WIP exercice javascript

--- Exercice/javascript/Ex_1.js
+++ Exercice/javascript/Ex_1.js
@@ -2,7 +2,18 @@ document.addEventListener("DOMContentLoaded",init);
 function fdiv_1() {
     div1.classList.add("div1")
 }
-
-function init {
-    div1.add.EventListnener("click", fdiv_1);
+function fdiv_2() {
+    div2.classList.add("div2")
+}
+function fdiv_3() {
+    div3.classList.add("div3")
+}
+function f_4() {
+    div3.classList.remove("div3")
+}
+function init() {
+    div1.addEventListener("click", fdiv_1);
+    div2.addEventListener("click", fdiv_2);
+    div3.addEventListener("mouseover", fdiv_3);
+    div3.addEventListener("mouseleave", f_4);
 }
