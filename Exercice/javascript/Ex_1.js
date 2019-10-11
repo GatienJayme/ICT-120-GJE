cbc6f33f0b81ce2c70d2a4c6b3482abacda9fe66
GatienJayme <gatien.jayme@cpnv.ch>
2019-10-11 16:53:23 +0200

    Ex_6_finis

--- Exercice/javascript/Ex_1.js
+++ Exercice/javascript/Ex_1.js
@@ -1,23 +1,27 @@
-document.addEventListener("DOMContentLoaded",init);
+document.addEventListener("DOMContentLoaded", init);
+
 function fdiv_1() {
     div1.classList.add("div1")
 }
+
 function fdiv_2() {
     div2.classList.add("div2")
 }
+
 function fdiv_3() {
     div3.classList.add("div3")
 }
+
 function f_4() {
     div3.classList.remove("div3")
 }
+
 function fdiv_4() {
-    if(chk1.checked == true && chk2.checked == true){
+    if (chk1.checked == true && chk2.checked == true) {
         console.log("div4");
         div4.classList.add("div4")
         div4.innerHTML = "YES";
-    }
-    else {
+    } else {
         console.log("NO");
     }
 }
@@ -26,9 +30,15 @@ function fdiv_5() {
     div5.classList.remove()
     div5.innerHTML = ""
 }
-function fdiv_6() {
-    div6.txtName.value.substr(0,1)
 
+function fdiv_6() {
+    var pre_let = txtName.value.substr(0, 1)
+    var x = txtName.value.lastIndexOf(" ")
+    var mi_let = txtName.value.substr(x + 1, 1)
+    var y = txtName.value.length
+    var der_let = txtName.value.substr(y - 1, 1)
+    var tot = pre_let + mi_let + der_let
+    txtInitials.value = tot.toUpperCase()
 }
 
 function init() {
@@ -39,5 +49,5 @@ function init() {
     chk1.addEventListener("click", fdiv_4);
     chk2.addEventListener("click", fdiv_4);
     div5.addEventListener("click", fdiv_5);
-    div6.addEventListener("keyup", fdiv_6);
+    txtName.addEventListener("keyup", fdiv_6);
 }
